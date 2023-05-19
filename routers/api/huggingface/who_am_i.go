db08562a2c3d0adf8e31b384fb2d697746ad3c09
Qunfei Wu <wu.qunfei@gmail.com>
2023-05-19 12:35:01 +0200

    add dummy implementation

--- routers/api/huggingface/who_am_i.go
+++ routers/api/huggingface/who_am_i.go
@@ -12,19 +12,20 @@ func WhoAMIHandler(ctx *context.APIContext) {
 
 	//todo how to getWhoAmIResponseByToken, mapping into this response
 	log.Debug(token)
+	user := ctx.Doer
 
 	whoAmIResponse := api.WhoAmIResponse{
-		Id:            "63d46a7c1da8c28b7b8abb0a",
+		Id:            string(user.ID),
 		Type:          "user",
-		Name:          "qunfei",
-		FullName:      "Qunfei Wu",
-		Email:         "wu.qunfei@gmail.com",
+		Name:          user.Name,
+		FullName:      user.FullName,
+		Email:         user.Email,
 		CanPay:        false,
 		Plan:          "NO_PLAN",
 		AvatarUrl:     "https://aeiljuispo.cloudimg.io/v7/https://s3.amazonaws.com/moonup/production/uploads/1674866533852-63d46a7c1da8c28b7b8abb0a.png?w=200&h=200&f=face",
 		PeriodEnd:     0,
 		Auth:          api.AuthResponse{},
-		EmailVerified: true,
+		EmailVerified: user.IsActive,
 		IsPro:         false,
 		Orgs:          nil,
 	}
